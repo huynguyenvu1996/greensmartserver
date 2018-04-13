30d5b8e92da11f5de623973cbef6aa033e02bdda
huynguyenvu <huynguyen@pakme.co>
2018-04-13 12:48:01 +0700

    Release server beta version!

--- models/agriculturalProductModel.js
+++ models/agriculturalProductModel.js
@@ -29,273 +29,271 @@ let db = databaseUtils.getConnect()
 
 class AgriculturalProduct {
 
-  constructor (
-    _id, name, image, temp_min, temp_max, humidity_min, humidity_max,
-    detect_rain, drying, notification, created_at, _rev) {
-    this._id = _id
-    this.name = name
-    this.image = image
-    this.temp_min = temp_min
-    this.temp_max = temp_max
-    this.humidity_min = humidity_min
-    this.humidity_max = humidity_max
-    this.detect_rain = detect_rain
-    this.drying = drying
-    this.notification = notification
-    this.created_at = created_at
-    this._rev = _rev
-  }
+    constructor(_id, name, image, temp_min, temp_max, humidity_min, humidity_max,
+                detect_rain, drying, notification, created_at, _rev) {
+        this._id = _id
+        this.name = name
+        this.image = image
+        this.temp_min = temp_min
+        this.temp_max = temp_max
+        this.humidity_min = humidity_min
+        this.humidity_max = humidity_max
+        this.detect_rain = detect_rain
+        this.drying = drying
+        this.notification = notification
+        this.created_at = created_at
+        this._rev = _rev
+    }
 
-  static parseAGPFromDatabase (result) {
-    try {
-      return new AgriculturalProduct(
-        result._id,
-        result.name,
-        result.image,
-        parseInt(result.temp_min),
-        parseInt(result.temp_max),
-        parseInt(result.humidity_min),
-        parseInt(result.humidity_max),
-        result.detect_rain,
-        result.drying,
-        result.notification,
-        result.created_at,
-        result._rev,
-      )
+    static parseAGPFromDatabase(result) {
+        try {
+            return new AgriculturalProduct(
+                result._id,
+                result.name,
+                result.image,
+                parseInt(result.temp_min),
+                parseInt(result.temp_max),
+                parseInt(result.humidity_min),
+                parseInt(result.humidity_max),
+                result.detect_rain,
+                result.drying,
+                result.notification,
+                result.created_at,
+                result._rev,
+            )
+        }
+        catch (e) {
+            return dataUtils.createErrorInstance('ParseAGPFromDatabase: ' +
+                e.message)
+        }
     }
-    catch (e) {
-      return dataUtils.createErrorInstance('ParseAGPFromDatabase: ' +
-        e.message)
-    }
-  }
 
-  static parseListAGPFromDatabase (result) {
-    try {
-      let listAGP = []
-      result.docs.forEach((value) => {
-        listAGP.push(AgriculturalProduct.parseAGPFromDatabase(value))
-      })
-      return listAGP
-    } catch (e) {
-      return dataUtils.createErrorInstance('ParseListAGPFromDatabase: ' +
-        e.message)
+    static parseListAGPFromDatabase(result) {
+        try {
+            let listAGP = []
+            result.docs.forEach((value) => {
+                listAGP.push(AgriculturalProduct.parseAGPFromDatabase(value))
+            })
+            return listAGP
+        } catch (e) {
+            return dataUtils.createErrorInstance('ParseListAGPFromDatabase: ' +
+                e.message)
+        }
     }
-  }
 
-  static parseAGPFromRequest (req) {
-    try {
-      let agp = new AgriculturalProduct(
-        req._id,
-        req.name,
-        req.image,
-        parseInt(req.temp_min),
-        parseInt(req.temp_max),
-        parseInt(req.humidity_min),
-        parseInt(req.humidity_max),
-        req.detect_rain,
-        req.drying,
-        req.notification,
-        req.created_at,
-        req._rev,
-      )
-      return dataUtils.createSuccessInstance(agp, 1)
+    static parseAGPFromRequest(req) {
+        try {
+            let agp = new AgriculturalProduct(
+                req._id,
+                req.name,
+                req.image,
+                parseInt(req.temp_min),
+                parseInt(req.temp_max),
+                parseInt(req.humidity_min),
+                parseInt(req.humidity_max),
+                req.detect_rain,
+                req.drying,
+                req.notification,
+                req.created_at,
+                req._rev,
+            )
+            return dataUtils.createSuccessInstance(agp, 1)
 
+        }
+        catch (e) {
+            return dataUtils.createErrorInstance('ParseAGPFromDatabase: ' +
+                e.message)
+        }
     }
-    catch (e) {
-      return dataUtils.createErrorInstance('ParseAGPFromDatabase: ' +
-        e.message)
-    }
-  }
 
 }
 
 let isWeatherCompatible = (agp, weather) => {
-  // console.log('---------------------------------');
-  // console.log(agp);
-  // console.log(weather);
-  return !!((weather.temperature >= agp.temp_min && weather.temperature <=
-    agp.temp_max) &&
-    (weather.humidity >= agp.humidity_min && weather.humidity <=
-      agp.humidity_max) && (!weather.rain)
-    (dateUtils.isHourInterval(weather.dt, 6, 16)))
+    // console.log('---------------------------------');
+    // console.log(agp);
+    // console.log(weather);
+    return !!((weather.temperature >= agp.temp_min && weather.temperature <=
+        agp.temp_max) &&
+        (weather.humidity >= agp.humidity_min && weather.humidity <=
+            agp.humidity_max) &&
+        (dateUtils.isHourInterval(weather.dt, 6, 16)))
 
 }
 
 module.exports = {
-  /*https://github.com/pouchdb/pouchdb/issues/6399*/
-  /*https://github.com/nolanlawson/pouchdb-find/issues/27*/
-  getListAGP: async (params) => {
-    let listAGP = null, e = null
+    /*https://github.com/pouchdb/pouchdb/issues/6399*/
+    /*https://github.com/nolanlawson/pouchdb-find/issues/27*/
+    getListAGP: async (params) => {
+        let listAGP = null, e = null
 
-    await db.createIndex({
-      index: {
-        fields: ['created_at', 'type'],
-      },
-    }).then((result) => {
-    }).catch((error) => {
-      e = databaseUtils.parseError(error)
-    })
+        await db.createIndex({
+            index: {
+                fields: ['created_at', 'type'],
+            },
+        }).then((result) => {
+        }).catch((error) => {
+            e = databaseUtils.parseError(error)
+        })
 
-    if (_.isNull(e)) {
-      await db.find({
-        selector: {
-          type: docType,
-          created_at: {$gt: true},
-        },
-        sort: [{created_at: 'desc'}],
-      }, (error, result) => {
-        if (error) {
-          e = databaseUtils.parseError(error)
+        if (_.isNull(e)) {
+            await db.find({
+                selector: {
+                    type: docType,
+                    created_at: {$gt: true},
+                },
+                sort: [{created_at: 'desc'}],
+            }, (error, result) => {
+                if (error) {
+                    e = databaseUtils.parseError(error)
+                }
+                else {
+                    let data = AgriculturalProduct.parseListAGPFromDatabase(result)
+                    listAGP = dataUtils.createSuccessInstance(data, data.length)
+                }
+            })
         }
-        else {
-          let data = AgriculturalProduct.parseListAGPFromDatabase(result)
-          listAGP = dataUtils.createSuccessInstance(data, data.length)
+        return new Promise((resolve, reject) => {
+            !_.isNull(listAGP) ? resolve(listAGP) : reject(error)
+        })
+    },
+    listAGP: async (params) => {
+        let listAGP = null, e = null
+
+        await db.createIndex({
+            index: {
+                fields: ['created_at', 'type'],
+            },
+        }).then((result) => {
+        }).catch((error) => {
+            e = databaseUtils.parseError(error)
+        })
+
+        if (_.isNull(e)) {
+            await db.find({
+                selector: {
+                    type: docType,
+                    created_at: {$gt: true},
+                },
+                sort: [{created_at: 'desc'}],
+            }, (error, result) => {
+                if (error) {
+                    e = databaseUtils.parseError(error)
+                }
+                else {
+                    listAGP = AgriculturalProduct.parseListAGPFromDatabase(result)
+                }
+            })
         }
-      })
-    }
-    return new Promise((resolve, reject) => {
-      !_.isNull(listAGP) ? resolve(listAGP) : reject(error)
-    })
-  },
-  listAGP: async (params) => {
-    let listAGP = null, e = null
-
-    await db.createIndex({
-      index: {
-        fields: ['created_at', 'type'],
-      },
-    }).then((result) => {
-    }).catch((error) => {
-      e = databaseUtils.parseError(error)
-    })
-
-    if (_.isNull(e)) {
-      await db.find({
-        selector: {
-          type: docType,
-          created_at: {$gt: true},
-        },
-        sort: [{created_at: 'desc'}],
-      }, (error, result) => {
-        if (error) {
-          e = databaseUtils.parseError(error)
+        return new Promise((resolve, reject) => {
+            !_.isNull(listAGP) ? resolve(listAGP) : reject(error)
+        })
+    },
+    viewAGPInfo: async (_id) => {
+        let agp = null, e = null
+        await db.get(_id).then((doc) => {
+            let data = AgriculturalProduct.parseAGPFromDatabase(doc)
+            agp = dataUtils.createSuccessInstance(data, 0)
+        }).catch((error) => {
+            e = databaseUtils.parseError(error)
+        })
+        return new Promise((resolve, reject) => {
+            !_.isNull(agp) ? resolve(agp) : reject(e)
+        })
+    },
+    createAGP: async (data) => {
+        let res = null, e = null
+        let agp = null
+        try {
+            agp = {
+                _id: stringUtils.getUniqueId(),
+                name: data.name,
+                image: data.image,
+                temp_min: parseInt(data.temp_min),
+                temp_max: parseInt(data.temp_max),
+                humidity_min: parseInt(data.humidity_min),
+                humidity_max: parseInt(data.humidity_max),
+                detect_rain: data.detect_rain,
+                drying: data.drying,
+                notification: data.notification,
+                created_at: Date.now().toString(),
+                type: docType,
+            }
+        } catch (error) {
+            dataUtils.createErrorInstance('CreateAGP: ' + error.message)
         }
-        else {
-          listAGP = AgriculturalProduct.parseListAGPFromDatabase(result)
+        await db.put(agp).then(function (response) {
+            res = dataUtils.createBasicSuccessInstance()
+        }).catch(function (error) {
+            e = databaseUtils.parseError(error)
+        })
+
+        return new Promise((resolve, reject) => {
+            !_.isNull(res) ? resolve(res) : reject(e)
+        })
+    },
+    updateAGP: async (data) => {
+        let res = null, e = null
+        let agp = {
+            _id: data._id,
+            name: data.name,
+            image: data.image,
+            temp_min: parseInt(data.temp_min),
+            temp_max: parseInt(data.temp_max),
+            humidity_min: parseInt(data.humidity_min),
+            humidity_max: parseInt(data.humidity_max),
+            detect_rain: data.detect_rain,
+            drying: data.drying,
+            notification: data.notification,
+            created_at: Date.now().toString(),
+            type: docType,
+            _rev: data._rev,
         }
-      })
-    }
-    return new Promise((resolve, reject) => {
-      !_.isNull(listAGP) ? resolve(listAGP) : reject(error)
-    })
-  },
-  viewAGPInfo: async (_id) => {
-    let agp = null, e = null
-    await db.get(_id).then((doc) => {
-      let data = AgriculturalProduct.parseAGPFromDatabase(doc)
-      agp = dataUtils.createSuccessInstance(data, 0)
-    }).catch((error) => {
-      e = databaseUtils.parseError(error)
-    })
-    return new Promise((resolve, reject) => {
-      !_.isNull(agp) ? resolve(agp) : reject(e)
-    })
-  },
-  createAGP: async (data) => {
-    let res = null, e = null
-    let agp = null
-    try {
-      agp = {
-        _id: stringUtils.getUniqueId(),
-        name: data.name,
-        image: data.image,
-        temp_min: parseInt(data.temp_min),
-        temp_max: parseInt(data.temp_max),
-        humidity_min: parseInt(data.humidity_min),
-        humidity_max: parseInt(data.humidity_max),
-        detect_rain: data.detect_rain,
-        drying: data.drying,
-        notification: data.notification,
-        created_at: Date.now().toString(),
-        type: docType,
-      }
-    } catch (error) {
-      dataUtils.createErrorInstance('CreateAGP: ' + error.message)
-    }
-    await db.put(agp).then(function (response) {
-      res = dataUtils.createBasicSuccessInstance()
-    }).catch(function (error) {
-      e = databaseUtils.parseError(error)
-    })
 
-    return new Promise((resolve, reject) => {
-      !_.isNull(res) ? resolve(res) : reject(e)
-    })
-  },
-  updateAGP: async (data) => {
-    let res = null, e = null
-    let agp = {
-      _id: data._id,
-      name: data.name,
-      image: data.image,
-      temp_min: parseInt(data.temp_min),
-      temp_max: parseInt(data.temp_max),
-      humidity_min: parseInt(data.humidity_min),
-      humidity_max: parseInt(data.humidity_max),
-      detect_rain: data.detect_rain,
-      drying: data.drying,
-      notification: data.notification,
-      created_at: Date.now().toString(),
-      type: docType,
-      _rev: data._rev,
-    }
+        await db.put(agp).then(function (response) {
+            res = dataUtils.createBasicSuccessInstance()
+        }).catch(function (error) {
+            e = databaseUtils.parseError(error)
+        })
 
-    await db.put(agp).then(function (response) {
-      res = dataUtils.createBasicSuccessInstance()
-    }).catch(function (error) {
-      e = databaseUtils.parseError(error)
-    })
+        return new Promise((resolve, reject) => {
+            !_.isNull(e) ? reject(e) : resolve(res)
+        })
+    },
 
-    return new Promise((resolve, reject) => {
-      !_.isNull(e) ? reject(e) : resolve(res)
-    })
-  },
+    deleteAGP: async (agp) => {
+        let res = null, e = null
+        await db.remove(agp).then((response) => {
+            res = dataUtils.createBasicSuccessInstance()
+        }).catch((error) => {
+            e = databaseUtils.parseError(error)
+        })
+        return new Promise((resolve, reject) => {
+            !_.isNull(e) ? reject(e) : resolve(res)
+        })
+    },
 
-  deleteAGP: async (agp) => {
-    let res = null, e = null
-    await db.remove(agp).then((response) => {
-      res = dataUtils.createBasicSuccessInstance()
-    }).catch((error) => {
-      e = databaseUtils.parseError(error)
-    })
-    return new Promise((resolve, reject) => {
-      !_.isNull(e) ? reject(e) : resolve(res)
-    })
-  },
-
-  getCompatibleWeatherList: async (data, listWeather) => {
-    let listCompatibleWeather = [], res = null, e = null
-    let agpData = AgriculturalProduct.parseAGPFromRequest(data)
-    let agp = null
-    if (!dataUtils.isSuccessData(agpData)) {
-      e = dataUtils.createErrorInstance(
-        'GetCompatibleWeatherList: Can\'t parse data of agp!')
-    } else {
-      agp = dataUtils.getData(agpData)
-      listWeather.forEach((value) => {
-        if (isWeatherCompatible(agp, value)) {
-          value.icon = owmConfigs.image.url + value.icon + '.png'
-          listCompatibleWeather.push(value)
+    getCompatibleWeatherList: async (data, listWeather) => {
+        let listCompatibleWeather = [], res = null, e = null
+        let agpData = AgriculturalProduct.parseAGPFromRequest(data)
+        let agp = null
+        if (!dataUtils.isSuccessData(agpData)) {
+            e = dataUtils.createErrorInstance(
+                'GetCompatibleWeatherList: Can\'t parse data of agp!')
+        } else {
+            agp = dataUtils.getData(agpData)
+            listWeather.forEach((value) => {
+                if (isWeatherCompatible(agp, value)) {
+                    listCompatibleWeather.push(value)
+                }
+            })
         }
-      })
-    }
 
-    res = dataUtils.createSuccessInstance(listCompatibleWeather,
-      listCompatibleWeather.length)
+        res = dataUtils.createSuccessInstance(listCompatibleWeather,
+            listCompatibleWeather.length)
 
-    return new Promise((resolve, reject) => {
-      !_.isNull(e) ? reject(e) : resolve(res)
-    })
-  },
+        return new Promise((resolve, reject) => {
+            !_.isNull(e) ? reject(e) : resolve(res)
+        })
+    },
 
 }
